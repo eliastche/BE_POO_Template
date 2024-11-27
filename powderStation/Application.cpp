e8f2894e8a51b96735623a5822dfd9e37251894a
brogsterski <haakon.bogen@hotmail.com>
2024-11-27 10:26:12 +0100

    Updatet file structure. Alt er bare bra

---
+++ powderStation/Application.cpp
@@ -0,0 +1,32 @@
+/*********************************************************************
+ * @file  Apllication.cpp
+ * @author Haakon Bogen & Elias Tche
+ * @brief Fichier source de l'application
+ *********************************************************************/
+#include "Application.h"
+
+
+Application::Application()
+{
+  // Code
+  ; 
+}
+  
+Application::~Application()
+{
+  // Code
+  ;
+}  
+
+void Application::init(void)
+{
+  // Code
+    ;
+}
+
+
+void Application::run(void)
+{
+  // Code
+    ;
+}
